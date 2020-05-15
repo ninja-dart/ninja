ff94836cd5854aa4df2d3c5005efe8b03bb83e43
Ravi Teja Gudapati <tejainece@gmail.com>
2020-05-15 02:01:13 +0200

    ASN1Sequence decode

--- lib/formats/asn1/asn1.dart
+++ lib/formats/asn1/asn1.dart
@@ -1,6 +1,87 @@
 import 'dart:typed_data';
 
+import 'package:collection/collection.dart';
 import 'package:ninja/utils/big_int.dart';
+
+class MutableIterable implements Iterable<int> {
+  Iterable<int> _base;
+
+  set mutate(Iterable<int> value) {
+    _base = value;
+  }
+
+  MutableIterable(this._base);
+
+  bool any(bool test(int element)) => _base.any(test);
+
+  Iterable<T> cast<T>() => _base.cast<T>();
+
+  bool contains(Object element) => _base.contains(element);
+
+  int elementAt(int index) => _base.elementAt(index);
+
+  bool every(bool test(int element)) => _base.every(test);
+
+  Iterable<T> expand<T>(Iterable<T> f(int element)) => _base.expand(f);
+
+  int get first => _base.first;
+
+  int firstWhere(bool test(int element), {int orElse()}) =>
+      _base.firstWhere(test, orElse: orElse);
+
+  T fold<T>(T initialValue, T combine(T previousValue, int element)) =>
+      _base.fold(initialValue, combine);
+
+  Iterable<int> followedBy(Iterable<int> other) => _base.followedBy(other);
+
+  void forEach(void f(int element)) => _base.forEach(f);
+
+  bool get isEmpty => _base.isEmpty;
+
+  bool get isNotEmpty => _base.isNotEmpty;
+
+  Iterator<int> get iterator => _base.iterator;
+
+  String join([String separator = ""]) => _base.join(separator);
+
+  int get last => _base.last;
+
+  int lastWhere(bool test(int element), {int orElse()}) =>
+      _base.lastWhere(test, orElse: orElse);
+
+  int get length => _base.length;
+
+  Iterable<T> map<T>(T f(int element)) => _base.map(f);
+
+  int reduce(int combine(int value, int element)) => _base.reduce(combine);
+
+  @deprecated
+  Iterable<T> retype<T>() => cast<T>();
+
+  int get single => _base.single;
+
+  int singleWhere(bool test(int element), {int orElse()}) {
+    return _base.singleWhere(test, orElse: orElse);
+  }
+
+  Iterable<int> skip(int n) => _base.skip(n);
+
+  Iterable<int> skipWhile(bool test(int value)) => _base.skipWhile(test);
+
+  Iterable<int> take(int n) => _base.take(n);
+
+  Iterable<int> takeWhile(bool test(int value)) => _base.takeWhile(test);
+
+  List<int> toList({bool growable = true}) => _base.toList(growable: growable);
+
+  Set<int> toSet() => _base.toSet();
+
+  Iterable<int> where(bool test(int element)) => _base.where(test);
+
+  Iterable<T> whereType<T>() => _base.whereType<T>();
+
+  String toString() => _base.toString();
+}
 
 class ASN1Type {
   final int tag;
@@ -33,22 +114,6 @@ class ASN1Type {
   static const UTCTimeTag = 23;
 }
 
-class ASN1DecodedLength {
-  final BigInt length;
-
-  final int bytesRequired;
-
-  ASN1DecodedLength(this.length, this.bytesRequired);
-
-  int get lengthAsInt {
-    if(!length.isValidInt) {
-      throw Exception('Length too large');
-    }
-
-    return length.toInt();
-  }
-}
-
 abstract class ASN1Object {
   int get tag;
 
@@ -65,23 +130,26 @@ abstract class ASN1Object {
     ];
   }
 
-  static ASN1DecodedLength decodeLength(Iterable<int> input) {
-    if(input.isEmpty) {
+  static BigInt decodeLength(final MutableIterable input) {
+    if (input.isEmpty) {
       throw Exception('Invalid data');
     }
 
-    if((input.first & 0x80) == 0) {
-      return ASN1DecodedLength(BigInt.from(input.first), 1);
+    if ((input.first & 0x80) == 0) {
+      final ret = BigInt.from(input.first);
+      input.mutate = input.skip(1);
+      return ret;
     }
 
     int numBytes = input.first & 0x7F;
-    input = input.skip(1);
-    if(input.length < numBytes) {
+    input.mutate = input.skip(1);
+    if (input.length < numBytes) {
       throw Exception('Invalid data');
     }
     final ret = bytesToBigInt(input.take(numBytes));
 
-    return ASN1DecodedLength(ret, numBytes + 1);
+    input.mutate = input.skip(numBytes);
+    return ret;
   }
 
   static Uint8List pack(int tag, Iterable<int> content) {
@@ -96,7 +164,35 @@ abstract class ASN1Object {
 
     return ret;
   }
+
+  static ASN1Object parse(final MutableIterable bytes) {
+    if (bytes.isEmpty) {
+      throw Exception('Invalid data');
+    }
+
+    switch (bytes.first) {
+      case ASN1Type.sequenceTag:
+        return ASN1Sequence.parse(bytes);
+      case ASN1Type.booleanTag:
+        return ASN1Boolean.parse(bytes);
+      case ASN1Type.integerTag:
+        return ASN1Integer.parse(bytes);
+      case ASN1Type.nullTag:
+        return ASN1Null.parse(bytes);
+      case ASN1Type.octetStringTag:
+        return ASN1OctetString.parse(bytes);
+      case ASN1Type.objectIdentifierTag:
+        return ASN1ObjectIdentifier.parse(bytes);
+      case ASN1Type.bitStringTag:
+        return ASN1BitString.parse(bytes);
+      default:
+        throw UnimplementedError();
+    }
+  }
 }
+
+typedef ASN1Parser = ASN1Object Function(MutableIterable bytes,
+    {Map<int, dynamic> parsers});
 
 class ASN1Sequence implements ASN1Object {
   final int tag = ASN1Type.sequenceTag;
@@ -106,11 +202,60 @@ class ASN1Sequence implements ASN1Object {
   ASN1Sequence(Iterable<ASN1Object> children)
       : children = List<ASN1Object>.from(children);
 
+  factory ASN1Sequence.decode(Iterable<int> bytes) =>
+      parse(MutableIterable(bytes));
+
+  static ASN1Sequence parse(final MutableIterable bytes) {
+    if (bytes.length < 3) {
+      throw Exception('Invalid data!');
+    }
+
+    int tag = bytes.first;
+    if (tag != ASN1Type.sequenceTag) {
+      throw Exception('Invalid tag!');
+    }
+    bytes.mutate = bytes.skip(1);
+
+    if (bytes.first != 0x80) {
+      final lengthBigInt = ASN1Object.decodeLength(bytes);
+      int length = lengthBigInt.toInt();
+      if (length == 0) {
+        throw Exception('Invalid data');
+      }
+
+      if (bytes.length < length) {
+        throw Exception('Invalid data');
+      }
+
+      final contentBytes = MutableIterable(bytes.take(length));
+      bytes.mutate = bytes.skip(length);
+
+      final children = <ASN1Object>[];
+      while (contentBytes.isNotEmpty) {
+        children.add(ASN1Object.parse(contentBytes));
+      }
+
+      return ASN1Sequence(children);
+    }
+
+    // TODO
+
+    throw UnimplementedError('Indefinite length form is not supported yet');
+  }
+
   Uint8List encode() {
     List<int> content = children.map((e) => e.encode()).fold(
         <int>[], (previousValue, element) => previousValue..addAll(element));
 
     return ASN1Object.pack(tag, content);
+  }
+
+  bool operator ==(dynamic other) {
+    if (other is ASN1Sequence) {
+      return IterableEquality<ASN1Object>().equals(other.children, children);
+    }
+
+    return false;
   }
 }
 
@@ -121,23 +266,30 @@ class ASN1Boolean implements ASN1Object {
 
   ASN1Boolean(this.value);
 
-  factory ASN1Boolean.decode(Iterable<int> bytes) {
-    if(bytes.length < 3) {
+  factory ASN1Boolean.decode(final Iterable<int> bytes) =>
+      parse(MutableIterable(bytes));
+
+  static ASN1Boolean parse(final MutableIterable bytes) {
+    if (bytes.length < 3) {
       throw Exception('Invalid data!');
     }
 
     int tag = bytes.first;
-    if(tag != ASN1Type.booleanTag) {
+    if (tag != ASN1Type.booleanTag) {
       throw Exception('Invalid tag!');
     }
-    bytes = bytes.skip(1);
+    bytes.mutate = bytes.skip(1);
 
-    if(bytes.first != 1) {
+    if (bytes.first != 1) {
       throw Exception('Invalid length!');
     }
-    bytes = bytes.skip(1);
+    bytes.mutate = bytes.skip(1);
 
-    return ASN1Boolean(bytes.first != 0);
+    final ret = ASN1Boolean(bytes.first != 0);
+
+    bytes.mutate = bytes.skip(1);
+
+    return ret;
   }
 
   Uint8List encode() {
@@ -148,6 +300,12 @@ class ASN1Boolean implements ASN1Object {
 
     return ret;
   }
+
+  bool operator ==(dynamic other) {
+    if (other is ASN1Boolean) return other.value == this.value;
+
+    return false;
+  }
 }
 
 class ASN1Integer implements ASN1Object {
@@ -157,34 +315,37 @@ class ASN1Integer implements ASN1Object {
 
   ASN1Integer(this.value);
 
-  ASN1Integer.fromNum(num value): value = BigInt.from(value);
+  ASN1Integer.fromNum(num value) : value = BigInt.from(value);
 
-  factory ASN1Integer.decode(Iterable<int> bytes) {
-    if(bytes.length < 3) {
+  factory ASN1Integer.decode(final Iterable<int> bytes) =>
+      parse(MutableIterable(bytes));
+
+  static ASN1Integer parse(final MutableIterable bytes) {
+    if (bytes.length < 3) {
       throw Exception('Invalid data!');
     }
 
     int tag = bytes.first;
-    if(tag != ASN1Type.booleanTag) {
+    if (tag != ASN1Type.integerTag) {
       throw Exception('Invalid tag!');
     }
-    bytes = bytes.skip(1);
+    bytes.mutate = bytes.skip(1);
 
-    final decodedLength = ASN1Object.decodeLength(bytes);
-    int length = decodedLength.lengthAsInt;
-    if(length == 0) {
-      throw Exception('Invalid data');
-    }
-    bytes = bytes.skip(decodedLength.bytesRequired);
-
-    if(bytes.length < length) {
+    final lengthBigInt = ASN1Object.decodeLength(bytes);
+    int length = lengthBigInt.toInt();
+    if (length == 0) {
       throw Exception('Invalid data');
     }
 
-    bytes = bytes.take(length);
+    if (bytes.length < length) {
+      throw Exception('Invalid data');
+    }
 
-    BigInt value = bytesToBigInt(bytes);
-    if((bytes.first & 0x80) == 0) {
+    final contentBytes = bytes.take(length);
+    bytes.mutate = bytes.skip(length);
+
+    BigInt value = bytesToBigInt(contentBytes);
+    if ((contentBytes.first & 0x80) != 0) {
       value = value.toSigned(value.bitLength);
     }
 
@@ -196,6 +357,12 @@ class ASN1Integer implements ASN1Object {
         outLen: value.isNegative ? null : ((value.bitLength + 7) >> 3) + 1);
     return ASN1Object.pack(tag, content);
   }
+
+  bool operator ==(dynamic other) {
+    if (other is ASN1Integer) return other.value == this.value;
+
+    return false;
+  }
 }
 
 class ASN1Null implements ASN1Object {
@@ -203,20 +370,24 @@ class ASN1Null implements ASN1Object {
 
   const ASN1Null();
 
-  factory ASN1Null.decode(Iterable<int> bytes) {
-    if(bytes.length < 2) {
+  factory ASN1Null.decode(final Iterable<int> bytes) =>
+      parse(MutableIterable(bytes));
+
+  static ASN1Null parse(final MutableIterable bytes) {
+    if (bytes.length < 2) {
       throw Exception('Invalid data!');
     }
 
     int tag = bytes.first;
-    if(tag != ASN1Type.nullTag) {
+    if (tag != ASN1Type.nullTag) {
       throw Exception('Invalid tag!');
     }
-    bytes = bytes.skip(1);
+    bytes.mutate = bytes.skip(1);
 
-    if(bytes.first != 0) {
+    if (bytes.first != 0) {
       throw Exception('Invalid length!');
     }
+    bytes.mutate = bytes.skip(1);
 
     return ASN1Null();
   }
@@ -228,6 +399,10 @@ class ASN1Null implements ASN1Object {
 
     return ret;
   }
+
+  bool operator ==(dynamic other) {
+    return other is ASN1Null;
+  }
 }
 
 class ASN1OctetString implements ASN1Object {
@@ -237,8 +412,46 @@ class ASN1OctetString implements ASN1Object {
 
   ASN1OctetString(this.value);
 
+  factory ASN1OctetString.decode(final Iterable<int> bytes) =>
+      parse(MutableIterable(bytes));
+
+  static ASN1OctetString parse(final MutableIterable bytes) {
+    if (bytes.length < 2) {
+      throw Exception('Invalid data!');
+    }
+
+    int tag = bytes.first;
+    if (tag != ASN1Type.octetStringTag) {
+      throw Exception('Invalid tag!');
+    }
+    bytes.mutate = bytes.skip(1);
+
+    final lengthBigInt = ASN1Object.decodeLength(bytes);
+    int length = lengthBigInt.toInt();
+    if (length == 0) {
+      return ASN1OctetString(Uint8List(0));
+    }
+
+    if (bytes.length < length) {
+      throw Exception('Invalid data');
+    }
+
+    final contentBytes = bytes.take(length);
+    bytes.mutate = bytes.skip(length);
+
+    return ASN1OctetString(Uint8List.fromList(contentBytes.toList()));
+  }
+
   Uint8List encode() {
     return ASN1Object.pack(tag, value);
+  }
+
+  bool operator ==(dynamic other) {
+    if (other is ASN1OctetString) {
+      return IterableEquality<int>().equals(other.value, value);
+    }
+
+    return false;
   }
 }
 
@@ -262,7 +475,67 @@ class ASN1ObjectIdentifier implements ASN1Object {
     return ASN1ObjectIdentifier.fromList(components);
   }
 
-  int _encodeSubIdentifier(int value, ByteData output) {
+  factory ASN1ObjectIdentifier.decode(final Iterable<int> bytes) =>
+      parse(MutableIterable(bytes));
+
+  static ASN1ObjectIdentifier parse(final MutableIterable bytes) {
+    if (bytes.length < 2) {
+      throw Exception('Invalid data!');
+    }
+
+    int tag = bytes.first;
+    if (tag != ASN1Type.objectIdentifierTag) {
+      throw Exception('Invalid tag!');
+    }
+    bytes.mutate = bytes.skip(1);
+
+    final lengthBigInt = ASN1Object.decodeLength(bytes);
+    int length = lengthBigInt.toInt();
+    if (length == 0) {
+      throw Exception('Invalid data');
+    }
+
+    if (bytes.length < length) {
+      throw Exception('Invalid data');
+    }
+    final contentBytes = MutableIterable(bytes.take(length));
+    bytes.mutate = bytes.skip(length);
+
+    final subIdentifiers = <int>[0];
+
+    while (contentBytes.isNotEmpty) {
+      final decoded = _decodedSubIdentifier(contentBytes);
+      subIdentifiers.add(decoded);
+    }
+
+    subIdentifiers[0] = subIdentifiers[1] ~/ 40;
+    subIdentifiers[1] = subIdentifiers[1] % 40;
+
+    return ASN1ObjectIdentifier(Uint32List.fromList(subIdentifiers));
+  }
+
+  static int _decodedSubIdentifier(final MutableIterable input) {
+    int value = 0;
+
+    while (true) {
+      if (input.isEmpty) {
+        throw Exception('Invalid data');
+      }
+      final d = input.first;
+      input.mutate = input.skip(1);
+
+      value <<= 7;
+      value |= d & 0x7F;
+
+      if ((d & 0x80) == 0) {
+        break;
+      }
+    }
+
+    return value;
+  }
+
+  static int _encodeSubIdentifier(int value, ByteData output) {
     if (value != 0) {
       int numBytes = (value.bitLength / 7).ceil();
       // TODO check length
@@ -324,6 +597,17 @@ class ASN1ObjectIdentifier implements ASN1Object {
     final content = encodeContent();
     return ASN1Object.pack(tag, content);
   }
+
+  String get objectIdentifierAsString => objectIdentifier.join('.');
+
+  bool operator ==(dynamic other) {
+    if (other is ASN1ObjectIdentifier) {
+      return IterableEquality<int>()
+          .equals(other.objectIdentifier, objectIdentifier);
+    }
+
+    return false;
+  }
 }
 
 class ASN1BitString implements ASN1Object {
@@ -348,7 +632,47 @@ class ASN1BitString implements ASN1Object {
   ASN1BitString(this.bitString, {int unusedBits = 0})
       : _unusedBits = unusedBits;
 
+  factory ASN1BitString.decode(Iterable<int> bytes) =>
+      parse(MutableIterable(bytes));
+
+  static ASN1BitString parse(MutableIterable bytes) {
+    if (bytes.length < 3) {
+      throw Exception('Invalid data!');
+    }
+
+    int tag = bytes.first;
+    if (tag != ASN1Type.bitStringTag) {
+      throw Exception('Invalid tag!');
+    }
+    bytes.mutate = bytes.skip(1);
+
+    final lengthBigInt = ASN1Object.decodeLength(bytes);
+    int length = lengthBigInt.toInt();
+    if (length == 0) {
+      throw Exception('Invalid data');
+    }
+
+    if (bytes.length < length) {
+      throw Exception('Invalid data');
+    }
+    Iterable<int> contentBytes = bytes.take(length);
+    bytes.mutate = bytes.skip(length);
+
+    int unusedBytes = contentBytes.first;
+    contentBytes = contentBytes.skip(1);
+
+    if (contentBytes.isEmpty && unusedBytes != 0) {
+      throw Exception('Unused bytes is not 0 while bitstring is empty');
+    }
+
+    return ASN1BitString(Uint8List.fromList(contentBytes.toList()),
+        unusedBits: unusedBytes);
+  }
+
   Iterable<int> encodeContent() {
+    if (bitString.isEmpty && unusedBits != 0) {
+      throw Exception('Unused bytes is not 0 while bitstring is empty');
+    }
     return <int>[
       unusedBits,
       ...bitString,
@@ -358,6 +682,15 @@ class ASN1BitString implements ASN1Object {
   Uint8List encode() {
     Iterable<int> content = encodeContent();
     return ASN1Object.pack(tag, content);
+  }
+
+  bool operator ==(dynamic other) {
+    if (other is ASN1BitString) {
+      return other.unusedBits == unusedBits &&
+          IterableEquality<int>().equals(other.bitString, bitString);
+    }
+
+    return false;
   }
 }
 
--- test/formats/asn1/asn1_test.dart
+++ test/formats/asn1/asn1_test.dart
@@ -3,8 +3,6 @@ import 'package:ninja/ninja.dart';
 import 'package:ninja/utils/big_int.dart';
 import 'package:test/test.dart';
 
-import 'package:asn1lib/asn1lib.dart' as asn1;
-
 void main() {
   group('ASN1', () {
     final integer = BigInt.parse(
@@ -17,25 +15,47 @@ void main() {
           hexEncode(ASN1Sequence(
               [ASN1Integer(integer), ASN1Null(), ASN1Boolean(true)]).encode()),
           '3082010a0282010100a3595fd150ee7d21f051f6a20d8b9adabfa8fc4a3b9ac037fcc327261b519c019028a3f157172fd13f8bfa001be3c7f06f6e28ae28842da037cd54caebd14c8822b501d4a447f29b450506b444a1c80b5aab4140158ff5b68d9527e086d7d9a96b7d66fa0e811c2d4d1b25796ed8b56e04af345182efed1804916dbbfd1514358df90ccf2845fb2043248337abafdbbe5b307b16fb485dd6cf16bbd9fab1d1f99dfadee1e01b89d3a509c2845b400c37a26c7b4aa880232c1effa084fc5cbead313a4868a01883d98cbd19a8ad04ee130e92e7b2ea4d6036417fbd57649a4c3ee7d134dbd84881635404fa8819ed3fac47391eaa3e5232a95bd67c9ebc972ffb05000101ff');
+
+      expect(
+          ASN1Sequence.decode(hexDecode(
+                  '3082010a0282010100a3595fd150ee7d21f051f6a20d8b9adabfa8fc4a3b9ac037fcc327261b519c019028a3f157172fd13f8bfa001be3c7f06f6e28ae28842da037cd54caebd14c8822b501d4a447f29b450506b444a1c80b5aab4140158ff5b68d9527e086d7d9a96b7d66fa0e811c2d4d1b25796ed8b56e04af345182efed1804916dbbfd1514358df90ccf2845fb2043248337abafdbbe5b307b16fb485dd6cf16bbd9fab1d1f99dfadee1e01b89d3a509c2845b400c37a26c7b4aa880232c1effa084fc5cbead313a4868a01883d98cbd19a8ad04ee130e92e7b2ea4d6036417fbd57649a4c3ee7d134dbd84881635404fa8819ed3fac47391eaa3e5232a95bd67c9ebc972ffb05000101ff'))
+              .children,
+          [ASN1Integer(integer), ASN1Null(), ASN1Boolean(true)]);
     });
 
     test('ASN1Boolean.encode', () {
       expect(hexEncode(ASN1Boolean(false).encode()), '010100');
+      expect(ASN1Boolean.decode(hexDecode('010100')).value, false);
+
       expect(hexEncode(ASN1Boolean(true).encode()), '0101ff');
+      expect(ASN1Boolean.decode(hexDecode('0101ff')).value, true);
     });
 
     test('ASN1Integer.encode', () {
       expect(hexEncode(ASN1Integer(integer).encode()),
           '0282010100a3595fd150ee7d21f051f6a20d8b9adabfa8fc4a3b9ac037fcc327261b519c019028a3f157172fd13f8bfa001be3c7f06f6e28ae28842da037cd54caebd14c8822b501d4a447f29b450506b444a1c80b5aab4140158ff5b68d9527e086d7d9a96b7d66fa0e811c2d4d1b25796ed8b56e04af345182efed1804916dbbfd1514358df90ccf2845fb2043248337abafdbbe5b307b16fb485dd6cf16bbd9fab1d1f99dfadee1e01b89d3a509c2845b400c37a26c7b4aa880232c1effa084fc5cbead313a4868a01883d98cbd19a8ad04ee130e92e7b2ea4d6036417fbd57649a4c3ee7d134dbd84881635404fa8819ed3fac47391eaa3e5232a95bd67c9ebc972ffb');
+
+      expect(
+          ASN1Integer.decode(hexDecode(
+                  '0282010100a3595fd150ee7d21f051f6a20d8b9adabfa8fc4a3b9ac037fcc327261b519c019028a3f157172fd13f8bfa001be3c7f06f6e28ae28842da037cd54caebd14c8822b501d4a447f29b450506b444a1c80b5aab4140158ff5b68d9527e086d7d9a96b7d66fa0e811c2d4d1b25796ed8b56e04af345182efed1804916dbbfd1514358df90ccf2845fb2043248337abafdbbe5b307b16fb485dd6cf16bbd9fab1d1f99dfadee1e01b89d3a509c2845b400c37a26c7b4aa880232c1effa084fc5cbead313a4868a01883d98cbd19a8ad04ee130e92e7b2ea4d6036417fbd57649a4c3ee7d134dbd84881635404fa8819ed3fac47391eaa3e5232a95bd67c9ebc972ffb'))
+              .value,
+          integer);
     });
 
     test('ASN1OctetString.encode', () {
       expect(hexEncode(ASN1OctetString(octet).encode()),
           '04820100a3595fd150ee7d21f051f6a20d8b9adabfa8fc4a3b9ac037fcc327261b519c019028a3f157172fd13f8bfa001be3c7f06f6e28ae28842da037cd54caebd14c8822b501d4a447f29b450506b444a1c80b5aab4140158ff5b68d9527e086d7d9a96b7d66fa0e811c2d4d1b25796ed8b56e04af345182efed1804916dbbfd1514358df90ccf2845fb2043248337abafdbbe5b307b16fb485dd6cf16bbd9fab1d1f99dfadee1e01b89d3a509c2845b400c37a26c7b4aa880232c1effa084fc5cbead313a4868a01883d98cbd19a8ad04ee130e92e7b2ea4d6036417fbd57649a4c3ee7d134dbd84881635404fa8819ed3fac47391eaa3e5232a95bd67c9ebc972ffb');
+
+      expect(
+          ASN1OctetString.decode(hexDecode(
+                  '04820100a3595fd150ee7d21f051f6a20d8b9adabfa8fc4a3b9ac037fcc327261b519c019028a3f157172fd13f8bfa001be3c7f06f6e28ae28842da037cd54caebd14c8822b501d4a447f29b450506b444a1c80b5aab4140158ff5b68d9527e086d7d9a96b7d66fa0e811c2d4d1b25796ed8b56e04af345182efed1804916dbbfd1514358df90ccf2845fb2043248337abafdbbe5b307b16fb485dd6cf16bbd9fab1d1f99dfadee1e01b89d3a509c2845b400c37a26c7b4aa880232c1effa084fc5cbead313a4868a01883d98cbd19a8ad04ee130e92e7b2ea4d6036417fbd57649a4c3ee7d134dbd84881635404fa8819ed3fac47391eaa3e5232a95bd67c9ebc972ffb'))
+              .value,
+          octet);
     });
 
     test('ASN1Null.encode', () {
       expect(hexEncode(ASN1Null().encode()), '0500');
+      ASN1Null.decode(hexDecode('0500'));
     });
 
     test('ASN1ObjectIdentifier.encode', () {
@@ -43,11 +63,23 @@ void main() {
           hexEncode(
               ASN1ObjectIdentifier.fromString('1.25.140.134569').encode()),
           '060641810c889b29');
+
+      expect(
+          ASN1ObjectIdentifier.decode(hexDecode('060641810c889b29'))
+              .objectIdentifierAsString,
+          '1.25.140.134569');
     });
 
     test('ASN1BitString.encode', () {
       expect(hexEncode(ASN1BitString(octet, unusedBits: 4).encode()),
           '0382010104a3595fd150ee7d21f051f6a20d8b9adabfa8fc4a3b9ac037fcc327261b519c019028a3f157172fd13f8bfa001be3c7f06f6e28ae28842da037cd54caebd14c8822b501d4a447f29b450506b444a1c80b5aab4140158ff5b68d9527e086d7d9a96b7d66fa0e811c2d4d1b25796ed8b56e04af345182efed1804916dbbfd1514358df90ccf2845fb2043248337abafdbbe5b307b16fb485dd6cf16bbd9fab1d1f99dfadee1e01b89d3a509c2845b400c37a26c7b4aa880232c1effa084fc5cbead313a4868a01883d98cbd19a8ad04ee130e92e7b2ea4d6036417fbd57649a4c3ee7d134dbd84881635404fa8819ed3fac47391eaa3e5232a95bd67c9ebc972ffb');
+
+      {
+        final bitString = ASN1BitString.decode(hexDecode(
+            '0382010104a3595fd150ee7d21f051f6a20d8b9adabfa8fc4a3b9ac037fcc327261b519c019028a3f157172fd13f8bfa001be3c7f06f6e28ae28842da037cd54caebd14c8822b501d4a447f29b450506b444a1c80b5aab4140158ff5b68d9527e086d7d9a96b7d66fa0e811c2d4d1b25796ed8b56e04af345182efed1804916dbbfd1514358df90ccf2845fb2043248337abafdbbe5b307b16fb485dd6cf16bbd9fab1d1f99dfadee1e01b89d3a509c2845b400c37a26c7b4aa880232c1effa084fc5cbead313a4868a01883d98cbd19a8ad04ee130e92e7b2ea4d6036417fbd57649a4c3ee7d134dbd84881635404fa8819ed3fac47391eaa3e5232a95bd67c9ebc972ffb'));
+        expect(bitString.unusedBits, 4);
+        expect(bitString.bitString, octet);
+      }
     });
   });
 }
